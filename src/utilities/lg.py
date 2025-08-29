8ac4ba121d15ff12e7a731f5a0a5a42a66dc3f0c
permalik <permalik@protonmail.com>
2025-08-28 22:20:56 -0500

    feat: fix local logging

--- src/utilities/lg.py
+++ src/utilities/lg.py
@@ -2,7 +2,7 @@ import os
 
 from loguru import logger
 
-log_dir = "/Users/tymalik/Docs/Git/lark-llm/logs"
+log_dir = "/Users/tymalik/Docs/Git/lark/lark-llm/logs"
 os.makedirs(log_dir, exist_ok=True)
 log_file = os.path.join(log_dir, "out.log")
 logger.remove()
