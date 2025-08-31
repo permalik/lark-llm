0870de14b310cef220408de4cb81abb1f54d650a
permalik <permalik@protonmail.com>
2025-08-31 15:36:08 -0500

    feat: update start script

--- start.sh
+++ start.sh
@@ -1,21 +1,35 @@
 #!/bin/sh
 
 TRUNCATE=$1
+VENV_DIR="venv"
+REQ_FILE="requirements.txt"
 
 if [ -z "$SHELLED" ]; then
     export SHELLED=1
     exec "$SHELL" "$0" "$@"
 fi
 
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment.."
+    python3 -m venv "$VENV_DIR"
+fi
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "Activating virtual environment.."
+    . "$VENV_DIR/bin/activate"
+fi
+
+if [ -f "$REQ_FILE" ]; then
+    echo "Checking/installing dependencies.."
+    pip install -r "$REQ_FILE"
+fi
+
 if [ "$TRUNCATE" = "y" ]; then
-    # TODO: check if venv is activated
-    . venv/bin/activate
     python3 ./src/main.py 2>&1 | sed 's/\x1b\[[0-9;]*m//g' > logs/out.log
 elif [ "$TRUNCATE" = "n" ]; then
-    # TODO: check if venv is activated
-    . venv/bin/activate
     python3 ./src/main.py 2>&1 | sed 's/\x1b\[[0-9;]*m//g' >> logs/out.log
 else
     echo "Usage: ./start.sh <y|n> (truncate logs)"
     exit 1
 fi
+
